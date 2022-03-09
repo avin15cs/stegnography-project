75291d25f7dff34b6a9e4c83b5683a8bfe84ca7f
avin15cs <avi.modi001@gmail.com>
2022-03-09 05:49:36 +0530

    Source code

---
+++ src/com/myproject/test.java
@@ -0,0 +1,27 @@
+package com.myproject;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class test extends JFrame
+{
+    Image newimg;
+    test()
+    {
+    }
+    test(Image m)
+    {
+        newimg=m;
+    }
+    public void paint(Graphics g)
+    {
+        g.drawImage(newimg,100,100,null);
+    }
+
+    public static void main(String args[])
+    {
+        test bp=new test();
+        bp.setSize(1035,740);
+        bp.setVisible(true);
+    }
+}
